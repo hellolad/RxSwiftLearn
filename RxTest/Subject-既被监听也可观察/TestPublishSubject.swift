d98f2cbf77a00dffcdc07d78ce99ce43715e74b2
赵恒 <heng.zhao@magicwindow.cn>
2018-03-19 15:10:10 +0800

    add PublishSubject

--- RxTest/Subject-既被监听也可观察/TestPublishSubject.swift
+++ RxTest/Subject-既被监听也可观察/TestPublishSubject.swift
@@ -6,9 +6,49 @@
 //  Copyright © 2018年 赵恒. All rights reserved.
 //
 
+/**
+ PublishSubject 将对观察者发送订阅后产生的元素，而在订阅前发出的元素将不会发送给观察者。
+ 如果源 Observable 因为产生了一个 error 事件而中止， PublishSubject 就不会发出任何元素，而是将这个 error 事件发送出来。
+ */
+
 import Foundation
 import RxSwift
 
 struct TestPublishSubject {
+    private static let bag = DisposeBag()
+    private static let subject = PublishSubject<String>()
     
+    static func test() {
+        subject
+            .subscribe(onNext: { str in
+                print("PublishSubject1 \(str)")
+            }, onError: { error in
+                print("PublishSubject1 error \(error)")
+            }, onCompleted: {
+                print("PublishSubject1 completed")
+            }, onDisposed: {
+                print("PublishSubject1 onDisposed")
+            })
+            .disposed(by: bag)
+        
+        subject.onNext("🐶")
+        subject.onNext("🐔")
+        
+        subject
+            .subscribe(onNext: { str in
+                print("PublishSubject2 \(str)")
+            }, onError: { error in
+                print("PublishSubject2 error \(error)")
+            }, onCompleted: {
+                print("PublishSubject2 completed")
+            }, onDisposed: {
+                print("PublishSubject2 onDisposed")
+            })
+            .disposed(by: bag)
+        
+        subject.onNext("🐯")
+        subject.onNext("🦁")
+        
+
+    }
 }
--- RxTest/ViewController.swift
+++ RxTest/ViewController.swift
@@ -69,6 +69,9 @@ class ViewController: UIViewController {
             TestAsyncSubject.test()
         }
         
+        of("PublishSubject") {
+            TestPublishSubject.test()
+        }
     }
 }
 
